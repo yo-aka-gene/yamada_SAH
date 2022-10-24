9ae4f582a5c0f99220df3821b4ed29894a7efa0e
yo-aka-gene <yujiokano@keio.jp>
2022-10-24 20:26:41 +0900

    :sparkles: add tools/preference

---
+++ tools/preference/__init__.py
@@ -0,0 +1,7 @@
+from ._figure import kwarg_savefig
+from ._gene import gene_micg
+
+__all__ = [
+    "kwarg_savefig",
+    "gene_micg"
+]
---
+++ tools/preference/_figure.py
@@ -0,0 +1,6 @@
+kwarg_savefig = {
+    "facecolor": "white",
+    "dpi": 300,
+    "bbox_inches": "tight",
+    "pad_inches": 0.05
+}
