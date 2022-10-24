dbc9550f9171cbc9660e9cae64d64604c249e2fe
yo-aka-gene <yujiokano@keio.jp>
2022-10-24 20:05:53 +0900

    :recycle: add pipeline to generate label matrix

--- tools/preprocessing/_pipeline.py
+++ tools/preprocessing/_pipeline.py
@@ -17,3 +17,10 @@ df.columns.name = "log2(RKPM+1)"
 df.index = [name.split("_")[0] for name in df.index]
 
 df.to_pickle("data/gse79416.pkl")
+
+label = {
+    "label_name": [name[:-1] for name in df.index],
+    "label_index": [0 if "CC" in name else 1 for name in df.index]
+}
+
+pd.DataFrame(label, index=df.index).to_pickle("data/gse79416_label.pkl")
